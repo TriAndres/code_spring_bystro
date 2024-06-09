5a1a7c8214af79de2ce787598ac98e6998a22c95
Andrei <An.S-1@yandex.ru>
2024-06-09 15:33:19 +0300

    sq-ch2-ex8

--- sb_ch1/src/main/java/org/example/main/Main.java
+++ sb_ch1/src/main/java/org/example/main/Main.java
@@ -3,9 +3,21 @@ package org.example.main;
 import org.example.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import java.util.function.Supplier;
+
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        Parrot x = new Parrot();
+        x.setName("kiki");
+
+        Supplier<Parrot> parrotSupplier = () -> x;
+
+        context.registerBean("parrot",
+                Parrot.class,
+                parrotSupplier,
+                bc -> bc.setPrimary(true));
 
         Parrot p = context.getBean(Parrot.class);
         System.out.println(p.getName());
