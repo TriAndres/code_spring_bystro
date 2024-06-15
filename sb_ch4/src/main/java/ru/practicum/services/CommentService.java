b40dff3b483976ab1a6bf6e4b1f7e8587917ced2
Andrei <An.S-1@yandex.ru>
2024-06-15 18:47:35 +0300

    sq-ch4-ex3

--- sb_ch4/src/main/java/ru/practicum/services/CommentService.java
+++ sb_ch4/src/main/java/ru/practicum/services/CommentService.java
@@ -1,18 +1,16 @@
 package ru.practicum.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import ru.practicum.model.Comment;
 import ru.practicum.proxies.CommentNotificationProxy;
 import ru.practicum.repositories.CommentRepository;
 @Component
 public class CommentService {
-    private final CommentRepository commentRepository;
-    private final CommentNotificationProxy commentNotificationProxy;
-
-    public CommentService(CommentRepository commentRepository, CommentNotificationProxy commentNotificationProxy) {
-        this.commentRepository = commentRepository;
-        this.commentNotificationProxy = commentNotificationProxy;
-    }
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private CommentNotificationProxy commentNotificationProxy;
 
     public void publishComment(Comment comment) {
         commentRepository.storeComment(comment);
