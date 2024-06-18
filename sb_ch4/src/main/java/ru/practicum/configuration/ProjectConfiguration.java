4df61579ff005c2bee0511403b9a0c60e29c754f
Andrei <An.S-1@yandex.ru>
2024-06-18 12:23:29 +0300

    sq-ch4-ex5

--- sb_ch4/src/main/java/ru/practicum/configuration/ProjectConfiguration.java
+++ sb_ch4/src/main/java/ru/practicum/configuration/ProjectConfiguration.java
@@ -1,14 +1,34 @@
 package ru.practicum.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+import ru.practicum.proxies.CommentNotificationProxy;
+import ru.practicum.proxies.EmailCommentNotificationProxy;
+import ru.practicum.repositories.CommentRepository;
+import ru.practicum.repositories.DBCommentRepository;
+import ru.practicum.services.CommentService;
 
 
 @Configuration
-@ComponentScan(
-        basePackages = {"ru.practicum.proxies", "ru.practicum.services", "ru.practicum.repositories"}
-)
 public class ProjectConfiguration {
+    @Bean
+    public CommentRepository commentRepository() {
+        return new DBCommentRepository();
+    }
+
+    @Bean
+    public CommentNotificationProxy commentNotificationProxy() {
+        return new EmailCommentNotificationProxy();
+    }
+
+    @Bean
+    public CommentService commentService(
+            CommentRepository commentRepository,
+            CommentNotificationProxy commentNotificationProxy
+    ) {
+        return new CommentService(commentRepository,
+                commentNotificationProxy);
+    }
 }
 
